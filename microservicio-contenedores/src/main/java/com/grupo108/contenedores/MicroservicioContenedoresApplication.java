43d28a513fc6895a191fa410a2810e27d643b0b1
julioMartin12 <rl_yoo@yahoo.com.ar>
2025-11-08 22:58:38 -0300

    creando la estructura de los servicios y configuración del pom

---
+++ microservicio-contenedores/src/main/java/com/grupo108/contenedores/MicroservicioContenedoresApplication.java
@@ -0,0 +1,12 @@
+package com.grupo108.contenedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioContenedoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioContenedoresApplication.class,args);
+    }
+}
